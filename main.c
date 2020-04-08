baf04893665bf1ee1406d0fcc64d548d5e25f9f6
logan42474 <loganmiller040402@gmail.com>
2020-04-07 22:36:26 -0700

    now takes input

--- main.c
+++ main.c
@@ -3,35 +3,35 @@ int change_rate = 1;
 int start = 100;
 int scale = 100;
 int main(){
-	DDRB = 0b000111;
+	DDRB = 0b000111; // sets 0-2 as outputs
+	PORTB |= (1 << 3); // enables pull-up resistor
     while (true) {
-		
-		if(PINB & (1<<3)){
-			change(1,2);
-		}else{
+		if(get_button(3)){
 			change(0,2);
-		}
-		/*
-		int delay = start;
-		for(int i=0;i<scale;i++){
-			delay-=change_rate;
-			change(1,0);
+			int delay = start;
+			for(int i=0;i<scale&&get_button(3);i++){
+				delay-=change_rate;
+				change(1,0);
+				change(0,1);
+        		delay_ms(delay);
+				change(0,0);
+				change(1,1);
+       			delay_ms(delay); 
+			}
+			for(int i=0;i<scale&&get_button(3);i++){
+				delay+=change_rate;
+				change(1,0);
+				change(0,1);
+        		delay_ms(delay);
+				change(0,0);
+				change(1,1);
+       			delay_ms(delay); 
+			}
+		}else{
+			change(1,2);
 			change(0,1);
-        	delay_ms(delay);
 			change(0,0);
-			change(1,1);
-       		delay_ms(delay); 
 		}
-		for(int i=0;i<scale;i++){
-			delay+=change_rate;
-			change(1,0);
-			change(0,1);
-        	delay_ms(delay);
-			change(0,0);
-			change(1,1);
-       		delay_ms(delay); 
-		}
-		*/
     } 
 }
 void delay_ms(int ms ){
@@ -46,3 +46,10 @@ void change(int x, int port){
 		PORTB |=(1<<port);
 	}
 }  
+get_button(int port){
+	if(!(PINB & (1<<port))){
+		return true;
+	}else{
+		return false;
+	}
+}
